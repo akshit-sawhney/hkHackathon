12b247f448c230cbd734692093cdcdd7f4d3c5de
Ayushi Gupta <Ayushi.Gupta@Brightlife.com>
2016-10-20 19:37:03 +0530

    logos

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
  
     $(".main-cat").on("click",function(eve){
-//        if($(this).hasClass(".ui-state-active")){
-//            $(this).find("i").removeClass("fa-chevron-up");
-//         $(this).find("i").addClass("fa-chevron-down");
-//    }
-//    else {
-//        $(this).find("i").removeClass("fa-chevron-down");
-//        $(this).find("i").addClass("fa-chevron-up");        
-//        
-//    }
+
          if($(this).attr("aria-expanded") == "false"){
              $(this).find("i").removeClass("fa-chevron-down");
         $(this).find("i").addClass("fa-chevron-up"); 
@@ -25,7 +17,7 @@ $(".shake").on("click",function(){
     $(".overlayContent").show();
 });
     $(".gotIt").on("click",function(){
-        $("body").removeClass("overlay");
+        $(".overlayContent").hide();
     });
     
 });
